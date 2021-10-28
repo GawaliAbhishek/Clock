004db56e29b06ef5dfba8bc446403b3c8baedcba
Abhishek Gawali <gawalibhskh@gmail.com>
2021-10-28 19:06:43 +0530

    modal js change

--- script.js
+++ script.js
@@ -22,3 +22,35 @@ function currentTime(){
 }
 
 setInterval(currentTime, 1000);
+
+// modal js
+//grt modal element
+var modal = document.getElementById("simplemodal");
+//get open modal button
+var modelBtn = document.getElementById("modal-btn");
+//get close button
+var closeBtn = document.getElementsByClassName("closebtn")[0];
+
+//listen for a openclick 
+modelBtn.addEventListener('click',openModal);
+//listem for close click
+closeBtn.addEventListener('click',closeModal);
+//listen for outside click
+window.addEventListener('click',clickOut);
+
+//funtion to open modal
+function openModal(){
+    modal.style.display = 'block';
+}
+
+//funtion to close modal
+function closeModal(){
+    modal.style.display = 'none';
+}
+
+//function to close the modal by clicking outside the modal
+
+function clickOut(e){
+    if(e.target == modal)
+        modal.style.display = 'none';
+}
